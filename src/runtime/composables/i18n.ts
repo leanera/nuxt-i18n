485cef236f5163615ef8c401d5c40225c01624db
Johann Schopplich <mail@johannschopplich.com>
2022-08-04 19:41:41 +0200

    refactor: composable return type

--- src/runtime/composables/i18n.ts
+++ src/runtime/composables/i18n.ts
@@ -2,5 +2,5 @@ import type { I18nInstance } from '@leanera/vue-i18n'
 import { useNuxtApp } from '#imports'
 
 export function useI18n() {
-  return useNuxtApp().$i18n as Omit<I18nInstance, 'install'>
+  return useNuxtApp().$i18n as Pick<I18nInstance, 'locales' | 'locale' | 'messages' | 't' | 'setLocale' | 'getLocale'>
 }
