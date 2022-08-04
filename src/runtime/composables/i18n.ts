9dbe1327daeada47bf7d27f205bb0a906e97676f
Johann Schopplich <mail@johannschopplich.com>
2022-08-04 15:52:26 +0200

    fix: import nuxt app composable from global imports

--- src/runtime/composables/i18n.ts
+++ src/runtime/composables/i18n.ts
@@ -1,4 +1,5 @@
 import type { I18nInstance } from '@leanera/vue-i18n'
+import { useNuxtApp } from '#imports'
 
 export function useI18n() {
   return useNuxtApp().$i18n as Omit<I18nInstance, 'install'>
