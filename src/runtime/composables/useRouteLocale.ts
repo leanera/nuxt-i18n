f4034029b64f1c04e4b41b8de431c944d9e78764
Johann Schopplich <mail@johannschopplich.com>
2022-12-21 13:23:16 +0100

    feat: pass custom route location to `useRouteLocale`

--- src/runtime/composables/useRouteLocale.ts
+++ src/runtime/composables/useRouteLocale.ts
@@ -1,6 +1,9 @@
+import type { RouteLocationNormalized, RouteLocationNormalizedLoaded } from 'vue-router'
 import { getLocaleFromRoute } from '../utils'
 import { useRoute } from '#imports'
 
-export function useRouteLocale() {
-  return getLocaleFromRoute(useRoute())
+export function useRouteLocale(
+  route: RouteLocationNormalizedLoaded | RouteLocationNormalized | string = useRoute(),
+) {
+  return getLocaleFromRoute(route)
 }
