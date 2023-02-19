3138e6ab8d617ed572f5d817c89f9e46842e80bd
Johann Schopplich <mail@johannschopplich.com>
2023-02-19 22:22:55 +0100

    test: add tests for `useLocalizedPath` & `useRouteLocale`

--- playground/pages/test/index.vue
+++ playground/pages/test/index.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 const { defaultLocale, locale, locales, t } = useI18n()
 
+const composables = {
+  useRouteLocale: useRouteLocale(),
+  useLocalizedPath: locales.map(locale => useLocalizedPath(useRoute().path, locale)),
+}
+
 const data = {
+  composables,
   defaultLocale,
   locale: locale.value,
-  locales: locales.values,
+  locales,
   message: t('menu.home'),
 }
 
